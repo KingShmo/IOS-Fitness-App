2b2eb31ceec1cda953bec63cbb4cd339f0fb26ae
The1AndOnlyJinja <60294317+The1AndOnlyJinja@users.noreply.github.com>
2022-04-10 18:55:54 -0400

    Added Diet Screen Components

--- Fitness App/DietViewController.swift
+++ Fitness App/DietViewController.swift
@@ -9,13 +9,48 @@ import UIKit
 
 class DietViewController: UIViewController {
 
+    @IBOutlet weak var MaxCalorieLabel: UILabel!
+    
+    @IBOutlet weak var CurrentCalorieLabel: UILabel!
+    
+    @IBOutlet weak var BreakfastFoodTextField: UITextField!
+    
+    @IBOutlet weak var BreakfastCalorieTextField: UITextField!
+    
+    @IBOutlet weak var LunchFoodTextField: UITextField!
+    
+    @IBOutlet weak var LunchCalorieTextField: UITextField!
+    
+    @IBOutlet weak var DinnerFoodTextField: UITextField!
+    
+    @IBOutlet weak var DinnerCalorieTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onBreakfastFood(_ sender: Any) {
+    }
+    
+    @IBAction func onBreakfastCalorie(_ sender: Any) {
+    }
+    
+    @IBAction func onLunchFood(_ sender: Any) {
+    }
+    
+    @IBAction func onLunchCalorie(_ sender: Any) {
+    }
+    
+    @IBAction func onDinnerFood(_ sender: Any) {
+    }
+    
+    @IBAction func onDinnerCalorie(_ sender: Any) {
+    }
+    
+    @IBAction func onViewFoodReport(_ sender: Any) {
+    }
     /*
     // MARK: - Navigation
 
